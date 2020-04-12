407328510bbe644d3ac596936125f36ebebf6a37
Federico Gimenez <federico.gimenez@gmail.com>
2020-04-12 12:18:25 +0200

    integration test

---
+++ scripts/integration-tests.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+source /scripts/common.sh
+source /scripts/bootstrap-helm.sh
+
+run_tests() {
+    echo Running tests...
+
+    wait_pod_ready harvester
+}
+
+teardown() {
+    helmfile delete --purge
+}
+
+main(){
+    if [ -z "$KEEP_W3F_HARVESTER" ]; then
+        trap teardown EXIT
+    fi
+
+    source /scripts/build-helmfile.sh
+
+    run_tests
+}
+
+main

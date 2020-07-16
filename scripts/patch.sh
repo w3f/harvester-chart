8ceed46b1af872d3024afd3ab78a9b9fb5b9cc29
pampatzoglou <15629182+pampatzoglou@users.noreply.github.com>
2020-07-16 15:29:20 +0300

    Update patch.sh

--- scripts/patch.sh
+++ scripts/patch.sh
@@ -3,16 +3,7 @@ set -e
 
 
 patch(){
-  local BRANCH=$1
-  local LATEST_UPSTREAM=$2
-  local GITHUB_BOT_TOKEN=$3
+ local LATEST_UPSTREAM=$1
 
-  local build_dir=$(mktemp -d)
-  local target_repo="https://web3bot:${GITHUB_BOT_TOKEN}@github.com/w3f/harvester-chart"
-
-  cd $build_dir
-  git clone "${target_repo}" .
-  git checkout -b "${BRANCH}"
-
-  sed -i "/tag:/c\  tag: $LATEST_UPSTREAM" helmfile.d/config/kusama-internal.yaml.gotmpl
+ sed -i "/tag:/c\  tag: $LATEST_UPSTREAM" helmfile.d/config/kusama-internal.yaml.gotmpl
 }
